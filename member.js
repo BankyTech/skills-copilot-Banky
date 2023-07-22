391724032e4b00f63c03514eb66499a1154ba8ee
Banketeshvar Narayan <bnarayan123@hotmail.com>
2023-07-22 15:32:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'ReactJS'];
+    var member = {
+        name: 'Nguyen Van A',
+        age: 20,
+        skills: skills
+    }
+    return member;
+}
